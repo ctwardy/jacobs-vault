3bf0fd7040a0bac2b08b501d84753bd41fdf9c09
Charles Twardy <925782+ctwardy@users.noreply.github.com>
2020-12-30 09:39:53 -0500

    Get README working. Update 01_HitTest.

--- jacobs_vault/_nbdev.py
+++ jacobs_vault/_nbdev.py
@@ -3,25 +3,21 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"Vault": "00_core.ipynb",
-         "DAY_FILE_PATH": "01_HitTest.ipynb",
          "COLUMNS": "01_HitTest.ipynb",
          "DTYPES": "01_HitTest.ipynb",
-         "dates": "01_HitTest.ipynb",
+         "DATE_COLS": "01_HitTest.ipynb",
          "load_day_file": "01_HitTest.ipynb",
-         "df": "01_HitTest.ipynb",
+         "DAY_FILE_PATH": "01_HitTest.ipynb",
          "test_skyfield": "01_HitTest.ipynb",
          "satellite_alt_az_days": "01_HitTest.ipynb",
-         "df_alt_az_days": "01_HitTest.ipynb",
          "hit_quality": "01_HitTest.ipynb",
-         "df_hit_quality": "01_HitTest.ipynb",
-         "df_alt_az_days_visible": "01_HitTest.ipynb",
-         "fig": "01_HitTest.ipynb"}
+         "viz": "01_HitTest.ipynb"}
 
-modules = ["vault.py",
+modules = ["core.py",
            "hit_test.py"]
 
-doc_url = "https://ctwardy.github.io/jacobs_vault/"
+doc_url = "https://cmorris-jacobs.github.io/jacobs-vault/"
 
-git_url = "https://github.com/ctwardy/jacobs_vault/tree/main/"
+git_url = "https://github.com/cmorris-jacobs/jacobs-vault/tree/main/"
 
 def custom_doc_links(name): return None
