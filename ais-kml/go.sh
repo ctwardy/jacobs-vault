ba1f916663bce42f345b49f0155855742061a50a
David MacKenzie <david.mackenzie@jacobs.com>
2021-01-02 10:02:49 -0500

    updates agg stats

--- ais-kml/go.sh
+++ ais-kml/go.sh
@@ -1,1 +1,3 @@
-spark-submit --driver-memory 12g --executor-memory 12g --executor-cores 2  --conf spark.kryoserializer.buffer.max=128m --conf spark.driver.maxResultSize=2g --py-files simpleKml.zip 1-HivetoKml.py 
+rm *.kml
+rm *.kmz
+spark-submit --driver-memory 12g --executor-memory 12g --executor-cores 2  --conf spark.kryoserializer.buffer.max=128m --conf spark.driver.maxResultSize=2g --py-files simpleKml.zip 1-HivetoKml-byShip.py
