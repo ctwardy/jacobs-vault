338afe2342cacdb36e6ea8a604e125b374d87351
David MacKenzie <david.mackenzie@jacobs.com>
2021-01-03 10:31:29 -0500

    generates kml/kmz

--- ais-kml/go.sh
+++ ais-kml/go.sh
@@ -1,3 +1,7 @@
 rm *.kml
 rm *.kmz
+#spark-submit --driver-memory 12g --executor-memory 12g --executor-cores 2  --conf spark.kryoserializer.buffer.max=128m --conf spark.driver.maxResultSize=2g --py-files simpleKml.zip 1-HivetoKml-tracks-byShip.py
+
+
 spark-submit --driver-memory 12g --executor-memory 12g --executor-cores 2  --conf spark.kryoserializer.buffer.max=128m --conf spark.driver.maxResultSize=2g --py-files simpleKml.zip 1-HivetoKml-byShip.py
+#zip tracks.zip *.kmz
