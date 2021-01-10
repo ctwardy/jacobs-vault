aa13a12b6386d04bd929557aab8b2d1e03c6c571
Kevin English <kenglish@staging.silverdale.test>
2021-01-09 21:05:09 -0500

    cleaning up moved files

--- scripts/sql/ais__stats1.sql
+++
@@ -1,1 +0,0 @@
-select count(*), min(LAT), max(LAT), min(LON), max(LON), min(SOG), max(SOG), min(COG), max(COG), min(Heading), max(Heading) from af_vault.ais;
--- scripts/sql/daily_sat_2015_2017__max_offsets.sql
+++
@@ -1,1 +0,0 @@
-select satellite, max( day - datediff(substring(dt, 1, 10), '1999-12-31') ) as offset from af_vault.daily_sat_2015_2017 group by satellite order by offset;
