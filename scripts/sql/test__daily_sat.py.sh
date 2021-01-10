905f310536bd9e2f7f51564eda3c8fd97db685f8
Kevin English <kenglish@staging.silverdale.test>
2021-01-09 21:07:53 -0500

    cleaned up non-sql moved files

--- scripts/sql/test__daily_sat.py.sh
+++
@@ -1,2 +0,0 @@
-hive -e 'select satellite, datediff(substring(dt, 1, 10), "1999-12-31") as day, ts, dt, line1, line2 from af_vault.tle_2015_2017 where satellite in ("10234", "10252", "10289") order by satellite, day;' > daily_sat_sample_input.tab
-python daily_sat.py < daily_sat_sample_input.tab
